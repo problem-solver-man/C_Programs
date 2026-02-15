7afb2cd5ada7195e760e14fdcdd7a6d9d4337279
problem-solver-man <visheshkarnabcd@gmail.com>
2026-02-15 23:53:29 +0530

    simple multiplication table

---
+++ multiplication_table.c
@@ -0,0 +1,35 @@
+/*
+This program gives multiplication table of an integer.
+Date: 15 Feb 2026
+Author: Vishesh
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // Variable declaration/initialization
+    int num, status = 1;
+
+    while (status == 1)
+    {
+        // Taking input from the user
+        printf("\n\nEnter an integer: ");
+        scanf("%d", &num);
+        printf("\nMultiplication Table of %d:\n", num);
+
+        // Showing output to the user
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("%d x %d = %d\n", num, i, num * i);
+        }
+
+        // Asking from the user to Re-Run the program
+        printf("\nWould you like to print another table?");
+        printf("\nEnter 1 for 'Yes' or anything else for 'No': ");
+        scanf("%d", &status);
+    }
+    return 0;
+}
+
+// Thanks for your time.
